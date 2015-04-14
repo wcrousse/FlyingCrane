aee2a2125be564bc9786e2dcf9be9f09a57d0ce0
unknown <wrousse@POR1100060L5802.uss.net>
2015-04-14 07:55:53 -0700

    began adding model classes

---
+++ src/Web.Api.Models/AttendenceList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Api.Models
+{
+    class AttendenceList
+    {
+        public long Id { get; set; }
+        public Venue Venue { get; set; }
+    }
+}
