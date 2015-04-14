7e2167e3d4237b1e361587ea0f9b2d2d1b2ebdff
unknown <wrousse@POR1100060L5802.uss.net>
2015-04-14 07:41:54 -0700

    Created the skeleton. Added dependencies

---
+++ src/Data/Entities/Venue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    class Venue
+    {
+        public virtual long Id { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string PhoneNumber { get; set; }
+        public virtual string Email { get; set; } 
+    }
+}
